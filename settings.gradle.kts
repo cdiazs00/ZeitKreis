16121ff4eeaf9542d7abd9558ba3f3dbaa7034b3
cdiazs00 <cdiazs00@elpuig.xeill.net>
2025-03-13 16:58:10 +0100

    Versión 0.1.7

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,7 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -19,5 +20,4 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "ZeitKreis"
-include(":app")
+rootProject.name
